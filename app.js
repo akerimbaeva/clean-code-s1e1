9750d5de7b3a8411ebaf979780dc56c7333d021b
akerimbaeva <akerimbaeva@gmail.com>
2022-05-30 18:02:38 +0600

    fix: add BEM classes according to rule2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -43,9 +43,9 @@ var createNewTaskElement=function(taskString){
     editInput.className="task text-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit-btn";
 
-    deleteButton.className="del";
+    deleteButton.className="del-btn";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className='del-btn-img'
     deleteButton.appendChild(deleteButtonImg);
@@ -87,7 +87,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".edit-btn");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
@@ -159,8 +159,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.del");
+    var editButton=taskListItem.querySelector("button.edit-btn");
+    var deleteButton=taskListItem.querySelector("button.del-btn");
 
 
     //Bind editTask to edit button.
